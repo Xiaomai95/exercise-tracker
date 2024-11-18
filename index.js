0ec9a03424e04dfd3a8c17024c27e6dbbc284740
Xiao Mai <ethanmyers1995@gmail.com>
2024-11-18 16:32:57 +0000

    feat: Completed all tests

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const { json } = require('express/lib/response')
 require('dotenv').config()
 
 app.use(express.urlencoded({ extended: true })); //allows us to access req.body in requests
@@ -60,7 +61,6 @@ app.post('/api/users/:_id/exercises', async (req, res) => { //:_id refers to use
   let exDate = req.body.date ? new Date(req.body.date).toDateString() : new Date().toDateString(); //format yyyy-mm-dd with toDateString()
   let id = req.params._id;
 
-
   let findUser = await User.findById(id); //add if statement in case user not found
 
   if (findUser) { //Check if User does exist
@@ -86,12 +86,62 @@ app.post('/api/users/:_id/exercises', async (req, res) => { //:_id refers to use
   } else {
     res.json({error: "User not found. Try enter a different ID."})
   }
+})
 
+
+
+app.get('/api/users/:_id/logs', async (req, res) => {
+  let id = req.params._id;
+  let from = req.query.from ? new Date(req.query.from) : new Date(0).toISOString(); //If no 'from' given, starts from as far back as Date() can go, converts to yyyy-mm-dd format
+  let to = req.query.to ? new Date(req.query.to) : new Date().toISOString(); //If no 'to' given, starts from current date, converts to yyyy-mm-dd format
+  let limitQuery = req.query.limit ? Number(req.query.limit) : 0; //If no limit given, defaults to 0.
+
+  let findUser = await User.findById(id).exec();
+  console.log(to)
   
+  if(findUser) {
+    try {
+      let id = findUser.id;
+      let usernameFound = findUser.username;
+      console.log("id", id)
+      let exercises = await Exercise.find(
+      {
+        username: usernameFound,
+        date: {
+        $gte: from,
+        $lte: to
+      }
+       })
+       .select('description duration date')
+       .limit(limitQuery)
+       .exec();
+       console.log("exercises", exercises)
+       let parsedDatesLog = exercises.map(exercise => {
+        return {
+          description: exercise.description,
+          duration: exercise.duration * 1,
+          date: new Date(exercise.date).toDateString(),
+        };
+      });
+    
+      return res.json({
+        _id: id,
+        username: usernameFound,
+        count: parsedDatesLog.length,
+        log: parsedDatesLog,
+      });
+    } catch (e) {
+      return res.json({error: "error"})
+    }
+    
+  } else {
+    return res.json({error: 'No user found'})
+  }
+
 
 })
 
-///Log:
+///Log response:
 // {
 //   username: "fcc_test",
 //   count: 1,
